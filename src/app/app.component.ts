aa0a5f571789ded13be76ffcb27ef5d366ba0a5a
Miguel Figueira <miguelachof11@gmail.com>
2017-08-20 18:42:49 -0400

    primera parte del tutorial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+export class Hero {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'app';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+  title = 'Tour of Heroes';
 }
