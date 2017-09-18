9cebf2b658240416a449b8d341c35146757c8746
Miguel Figueira <miguel.figueira@wikot.com>
2017-09-17 20:51:38 -0400

    last commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
+
 import { HeroService } from './hero.service';
 
-
 import { AppRoutingModule } from './app-routing.module';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
     HeroDetailComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroSearchComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
+    HttpModule
   ],
   providers: [HeroService],
   bootstrap: [AppComponent]
